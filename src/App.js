cc51c390c591a850cd384943e520a0e874c7a5d5
Steven Craig <stevencraig321@gmail.com>
2021-02-09 20:44:39 +0000

    meh

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Shop from "./Components/Shop";
 import ItemPage from "./Components/ItemPage";
@@ -7,13 +7,13 @@ import './Components/CSS/App.css'
 
 const App = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/shop" component={Shop} />
         <Route exact path="/item" component={ItemPage} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
